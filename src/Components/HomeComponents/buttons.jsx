74cd683718ed12e2c4dca00c76c1715102ff7477
Alex Bertrand <alex.m.bertrand@gmail.com>
2021-01-19 20:43:03 -0500

    set up auth0

--- src/Components/HomeComponents/buttons.jsx
+++ src/Components/HomeComponents/buttons.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Button from 'react-bootstrap/Button';
+import { useAuth0 } from '@auth0/auth0-react'
 
 function Buttons() {
+    const { loginWithRedirect } = useAuth0();
+    
     return(
         <Container>
-            <Button className="account" variant="light">Create Account</Button>
-            <Button className="account" variant="light">Log In</Button>
+            <Button className="account" variant="light" onClick={() => loginWithRedirect()}>Create Account</Button>
+            <Button className="account" variant="light" onClick={() => loginWithRedirect()}>Log In</Button>
         </Container>
     )
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { Auth0Provider } from '@auth0/auth0-react';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Auth0Provider
+        domain="dev-bd39pa1e.us.auth0.com"
+        clientId="m2evhdh1LE4uY4NQNsbDnrmr6ddSnSeP"
+        redirectUri={window.location.origin}
+    >
+        <App />
+    </Auth0Provider>,
+    document.getElementById('root')
+);
